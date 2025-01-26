7675065820ed44f7f20f192ad5624c5b57fb8409
Yuvrajshankar <yuvrajpandar2004@gmail.com>
2025-01-26 16:19:23 +0530

    new job role and resume added.

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import "./HeroSection.css";
 import { Bio } from '../../data/constants';
 import logo from "../../pro.png";
+import Typed from 'typed.js';
 
 function HeroSection() {
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: Bio.roles,
+            typeSpeed: 200,
+            loop: true,
+            loopCount: Infinity,
+            cursorChar: "|",
+            backSpeed: 50,
+        });
+        return () => {
+            typed.destroy();
+        }
+    }, []);
 
     const handleButtonClick = () => {
-        window.open('https://drive.google.com/file/d/1-8mcwIHBZokNZqhwcMZN1ORSSnLwt7Oj/view?usp=sharing', '_blank');
+        window.open('https://docs.google.com/document/d/1gciGtMcy0U7DEWtRuqUA2Zaf7oJwcPCcH-r5dgNAcUw/edit?usp=drive_link', '_blank');
     };
 
     return (
@@ -16,8 +32,9 @@ function HeroSection() {
 
                 <div className="left">
                     <h1>Hi, I am <br />{Bio.name}</h1>
-                    <h2>I am a
-                        <span> Full Stack Developer</span>
+                    <h2>I am a {" "}
+                        {/* <span id="typewriter">{" "}{Bio.roles}</span> */}
+                        <span ref={el}></span>
                     </h2>
                     <h3>{Bio.description}</h3>
                     <button className='resume' onClick={handleButtonClick}>Check Resume</button>
